52c4136fe8e9bba129eb7b5a15252d133bc1e119
mendelB <mendelblesofsky@gmail.com>
2016-05-31 09:29:54 -0400

    add test for new case

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,4 +1,8 @@
 describe '#speak_to_grandma' do
+  it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
+    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
+  end
+
   it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
     expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
   end
