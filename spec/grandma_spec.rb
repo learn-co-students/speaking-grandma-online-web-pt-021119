82f2b0d90ab6fd20752610dc067419e37ed06a0d
Morgan VanYperen <morgvanny@gmail.com>
2018-08-07 11:39:27 -0500

    Update grandma_spec.rb
    remove exact copies of tests

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -19,12 +19,4 @@ describe '#speak_to_grandma' do
     expect(speak_to_grandma('WHAT?')).to eq "NO, NOT SINCE 1938!"
   end
 
-  it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
-    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
-  end
- 
-  it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
-    expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
-  end
-
 end
