2161c497164227ed1e2392245353d15474019f14
Steven G. Harms <steven.harms@flatironschool.com>
2018-12-03 09:49:09 -0500

    Update solution to pass

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,13 @@
 # NO, NOT SINCE 1938!
 
 
+# However if you say 'I LOVE YOU GRANDMA!', she should respond with
+# 'I LOVE YOU TOO PUMPKIN!'
+
 def speak_to_grandma(phrase)
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  end
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   else
@@ -18,5 +24,3 @@ def speak_to_grandma(phrase)
   end
 end
 
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
